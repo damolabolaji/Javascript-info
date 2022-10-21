f93a7fabae0e8b283c569c5200e04912db077400
Bolaji Oyindamola <86451863+damolabolaji@users.noreply.github.com>
2022-10-21 21:24:18 +0100

    Update exercises.js

--- js/exercises.js
+++ js/exercises.js
@@ -67,3 +67,19 @@ let users = [ john, pete, mary ];
 
 console.log(usersMapped)
 
+//MAP TO OBJECTS
+let john = { name: "John", surname: "Smith", id: 1 };
+let pete = { name: "Pete", surname: "Hunt", id: 2 };
+let mary = { name: "Mary", surname: "Key", id: 3 };
+
+let users = [ john, pete, mary ];
+
+ let usersMapped = users.map(user => ({fullName: `${user.name} ${user.surname}`, id: user.id}));
+
+
+console.log(usersMapped)
+
+//MAP TO NAMES
+let names = users.map(user => user.name);
+console.log(names) //['John', 'Pete', 'Mary']
+
